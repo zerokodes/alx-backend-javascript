ee1c905b03619f7946c7eb947bf2525ad1ebf475
Nduka Henry Chisom <henrysam140@gmail.com>
2023-06-30 02:18:04 +0100

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,13 +1,13 @@
 /**
- * A classRoom class 
+ * Represents a class room.
  */
 export default class ClassRoom {
-    /**
-     * Creates a new ClassRoom
-     * @param {Number} maxStudentsSize - The maximum number of 
-     * students in the class
-     */
-constructor(maxStudentsSize) {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
