4865245660013cba6ee6d95eb0136dc56f2b9d93
Nduka Henry Chisom <henrysam140@gmail.com>
2023-06-30 02:56:51 +0100

    Delete 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++
@@ -1,8 +0,0 @@
-import ClassRoom from './0-classroom';
-/**
- * Creates an array of ClassRoom with a specific size
- * @return an array of ClassRoom
- */
-export default function initializeRooms() {
-    return [19, 20, 34].map((size) => new ClassRoom(size));
-}
