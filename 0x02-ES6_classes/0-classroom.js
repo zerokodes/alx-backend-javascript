5b50449e39478ffb65d82ee2e59d6986ac3608f2
zerokodes <henrysam140@gmail.com>
2023-06-30 01:07:18 +0100

    completed tasks

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * A classRoom class 
+ */
+export default class ClassRoom {
+    /**
+     * Creates a new ClassRoom
+     * @param {Number} maxStudentsSize - The maximum number of 
+     * students in the class
+     */
+constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+/**
+ * Creates an array of ClassRoom with a specific size
+ * @return an array of ClassRoom
+ */
+export default function initializeRooms() {
+    return [19, 20, 34].map((size) => new ClassRoom(size));
+}
