29a83725b3e30d0ab43e524ba8740f49f7dc3300
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-04 20:02:28 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,16 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const subStr = value.substring(startString.length);
+
+      if (subStr && subStr !== value) {
+        parts.push(subStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
