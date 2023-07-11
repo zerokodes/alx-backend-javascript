a96c0591cc6aa5b45c57c172c92cb1501ed263bc
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-11 04:35:46 +0100

    Update React.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,18 @@
+/// <reference path="Subjects.ts" />
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingReact?: number;
+    }
+  
+    export class ReactClass extends Subject{
+      getRequirements = () => 'Here is the list of requirements for React';
+  
+      getAvailableTeacher = () => {
+        if (!this.teacher?.experienceTeachingReact) return 'No available teacher';
+  
+        return `Available Teacher: ${this.teacher}`;
+      } 
+    }
+  }
