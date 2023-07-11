6bf46b6acf7cae93a29d39506c615fd3ac498217
Nduka Henry Chisom <henrysam140@gmail.com>
2023-07-11 04:36:25 +0100

    Update Subject.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+/// <reference path="Teacher.ts"/>
+
+namespace Subjects {
+    export class Subject {
+        teacher: Teacher;
+        setTeacher = (teacher: Teacher) => {
+            this.teacher = teacher;
+        }
+    }
+}
