61ee2ba0682825c66d54fa368e4acafd981eb43c
Nduka Henry Chisom <henrysam140@gmail.com>
2023-08-23 04:21:13 +0100

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,31 +1,53 @@
 const fs = require('fs');
 
-async function countStudents(path) {
-  let data;
-  try {
-    data = await fs.promises.readFile(path, 'utf8');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-  const students = data.split('\r\n').slice(1)
-    .map((student) => student.split(','))
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-    let fields = students.map(student => student.field);
-    let unique_fields = new Set(fields);
-    let students_by_field = {};
-    for (let field of unique_fields) {
-      students_by_field[field] = [];
-    }
-    for (let student of students) {
-        students_by_field[student.field].push(student.firstName);
-    }
-    console.log(students_by_field);
-    return students_by_field;
-}
+module.exports = function readDatabase(path) {
+  return new Promise(((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, paramsStudents) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
 
-module.exports = countStudents;
+      let students = paramsStudents;
+      // @ts-ignore
+      students = students.split('\n');
+      const headers = students.shift().split(',');
+
+      const groupingStudentsField = {};
+      const studentsObjects = [];
+
+      students.forEach((student) => {
+        if (student) {
+          const studentInfo = student.split(',');
+          const studentObject = {};
+
+          headers.forEach((header, index) => {
+            studentObject[header] = studentInfo[index];
+            if (header === 'field') {
+              if (groupingStudentsField[studentInfo[index]]) {
+                groupingStudentsField[studentInfo[index]].push(studentObject.firstname);
+              } else {
+                groupingStudentsField[studentInfo[index]] = [studentObject.firstname];
+              }
+            }
+          });
+          studentsObjects.push(studentObject);
+        }
+      });
+      const numberStudents = `Number of students: ${studentsObjects.length}`;
+
+      let response = `${numberStudents}\n`;
+      console.log(numberStudents);
+
+      for (const groupStudent in groupingStudentsField) {
+        if (groupingStudentsField[groupStudent]) {
+          const listStudents = groupingStudentsField[groupStudent];
+          const responseGroupStudents = `Number of students in ${groupStudent}: ${listStudents.length}. List: ${listStudents.join(', ')}`;
+          response += `${responseGroupStudents}\n`;
+          console.log(responseGroupStudents);
+        }
+      }
+      resolve(response);
+    });
+  }));
+};
